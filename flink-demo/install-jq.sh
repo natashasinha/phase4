b5f93e7d1a4cb8022d9a38973c21aafd470ee72c
Neil Buesing <neil@kineticedge.io>
2024-03-03 09:56:04 -0600

    updated install script.

--- flink-demo/install-jq.sh
+++ flink-demo/install-jq.sh
@@ -5,12 +5,32 @@ cd "$(dirname -- "$0")"
 
 cd ./bin
 
-#curl -s -L -o jq https://github.com/jqlang/jq/releases/download/jq-1.7.1/jq-macos-arm64
-#chmod +x ./jq
+BINARY=unknown
 
+if [ "$OS" = "Windows_NT" ]; then
+  BINARY=jq-windows-amd64.exe
+else
+  echo "$(uname -s)_$(uname -m)"
+  case "$(uname -s)_$(uname -m)" in
+    Darwin_arm64)
+      BINARY=jq-macos-arm64
+      ;;
+    Darwin_amd64)
+      BINARY=jq-macos-amd64
+      ;;
+    Linux_arm64)
+      BINARY=jq-linux-arm64
+      ;;
+    Linux_amd64)
+      BINARY=jq-linux-amd64
+      ;;
+  esac
+fi
 
-#https://github.com/jqlang/jq/releases/download/jq-1.7.1/jq-windows-amd64.exe
-#https://github.com/jqlang/jq/releases/download/jq-1.7.1/jq-macos-amd64
-#https://github.com/jqlang/jq/releases/download/jq-1.7.1/jq-linux-arm64
-#https://github.com/jqlang/jq/releases/download/jq-1.7.1/jq-linux-amd64
-
+if [ "$BINARY" != "unknown" ]; then
+  echo "installing jq version $BINARY"
+  curl -s -L -o jq "https://github.com/jqlang/jq/releases/download/jq-1.7.1/$BINARY"
+  chmod +x ./jq
+else
+  echo "unable to figure out operating system to know which binary to install."
+fi
