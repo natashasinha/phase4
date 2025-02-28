b68cf0ad803d921dc8fe2e15e89f1550cf94c616
Neil Buesing <neil@kineticedge.io>
2025-02-28 16:07:26 -0600

    fix location of datagen plugin.

--- flink-demo/install-connector.sh
+++ flink-demo/install-connector.sh
@@ -5,6 +5,7 @@ cd "$(dirname -- "$0")"
 
 cd ../kafka-1/connect-plugins
 
-curl -s -L -O https://d1i4a15mxbxib1.cloudfront.net/api/plugins/confluentinc/kafka-connect-datagen/versions/0.6.4/confluentinc-kafka-connect-datagen-0.6.4.zip
+curl -s -L -O https://hub-downloads.confluent.io/api/plugins/confluentinc/kafka-connect-datagen/versions/0.6.4/confluentinc-kafka-connect-datagen-0.6.4.zip
+
 
 unzip confluentinc-kafka-connect-datagen-0.6.4.zip
